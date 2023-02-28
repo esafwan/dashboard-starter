8c5b9c2516e7a95b1262040ef2821d3af2062af8
Shahzad Bin Shahjahan <shahzadbinshahjahan@gmail.com>
2023-02-28 18:45:59 +0530

    rtl toggle done

--- src/Layouts/Dashboard/Dashboard.jsx
+++ src/Layouts/Dashboard/Dashboard.jsx
@@ -1,11 +1,14 @@
+import {useState} from "react";
 import Header from "./../../components/Header";
 import Sidebar from "./Sidebar/Sidebar";
 import Content from "./../../components/Content";
 
 function Dashboard({children}) {
+  const [direction,setDirection]=useState("ltr");
+  document.dir=direction;
   return (
     <div className="mx-auto flex flex-col space-y-6">      
-      <Header />      
+      <Header setDirection={setDirection}/>
       <div id="content" className="grid gap-12 md:grid-cols-[210px_2fr]">                
         <Sidebar />        
         {children?children:<Content/>}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import {Link} from "react-router-dom";
 import AccountNav from "./../Layouts/Dashboard/AccountNav";
 import Menu from "./../Layouts/Dashboard/MobileNav/Menu";
 
-function Header() {
+function Header({setDirection}) {
+  // const handleSelect=()=>{
+  //   const select=document.querySelector("#language");
+  //   console.log(select.value);
+  // }
   return (
     <header className="sticky top-0 z-40 bg-white pz-4 ">
       <div className="flex h-16 items-center justify-between border-b border-b-slate-200 py-4 ">
@@ -25,9 +29,15 @@ function Header() {
             </Link>
           </nav>
         </div>
-        <span className="hidden md:flex">
-          <AccountNav />
-        </span>
+        <div className="flex">
+            <select className="mx-10" id="language" onChange={()=>setDirection(document.querySelector("#language").value)}>
+              <option value="ltr">english</option>
+              <option value="rtl">arabic</option>
+            </select>
+            <span className="hidden md:flex">
+              <AccountNav />
+            </span>
+        </div>
       </div>
     </header>
   );
