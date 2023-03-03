922eca7883b0086dabff5af2d11311d80528cfab
Shahzad Bin Shahjahan <shahzadbinshahjahan@gmail.com>
2023-03-03 21:15:03 +0530

    fixed firebase api issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-console.log(import.meta.env.VITE_apiKey);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
@@ -8,9 +8,8 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
-  measurementId:import.meta.env.VITE_mesaurementId
+  measurementId:import.meta.env.VITE_measurementId
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
