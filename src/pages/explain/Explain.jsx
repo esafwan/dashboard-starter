056dab7df61d56beb3ec48a3b361d46057b4cd9f
Shahzad Bin Shahjahan <shahzadbinshahjahan@gmail.com>
2023-03-06 04:58:09 +0530

    changes explain ui

--- src/pages/explain/Explain.jsx
+++ src/pages/explain/Explain.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import {useTranslation} from "react-i18next";
 import InputQuery from "./components/InputQuery";
 
 function Explain(){
+    const {t}=useTranslation();
     return (
-        <InputQuery/>
+        <div className="flex flex-col overflow-hidden h-full">
+            <div className="flex justify-between">
+                <div className="text-2xl font-bold tracking-wide text-slate-900">{t("Suggest")}</div>
+                {/* {(!loading && writeResponseText) && <MainButton text={t("Modify")} onClickHandler={resetHandler}/>} */}
+            </div>
+             <div className={"flex flex-col border border-black border-dashed rounded-md border-slate-200 mt-12"}>
+                {/* {loading && <LoadingScreen text={`${t("writing")}...`}/>}
+                {(!loading && writeResponseText) && 
+                <>
+                    <div className="p-2 self-end">
+                        <CopyButton text={writeResponseText}/>
+                    </div>
+                    <ResultScreen
+                    text={writeResponseText}
+                    setResponseText={setWriteResponseText}/>
+                </>}
+                {(!loading && !writeResponseText) && <InputQuery 
+                    submitQuery={submitQuery} />} */}
+                <InputQuery/>
+             </div>
+        </div>
     );
 }
 
